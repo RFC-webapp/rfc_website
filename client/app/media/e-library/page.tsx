63cdbdc156e5403eeaaa387acb20c2002c5e2cdd
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2025-12-17 15:29:20 +0100

    restructure and renaming

--- client/app/media/e-library/page.tsx
+++ client/app/media/e-library/page.tsx
@@ -1,4 +1,15 @@
-import LibraryCard from "@/components/media/MediaCard";
+import MediaCard from "@/components/media/MediaCard";
+import PaginatedContent from "@/components/media/PaginationContent";
+import Selection from "@/components/media/Selection";
+import TitleTag from "@/components/media/TitleTag";
+import {
+  DownloadIcon,
+  FaithIcon,
+  FinanceIcon,
+  HealingIcon,
+  MarriageIcon,
+  SearchIcon,
+} from "@/public/icons";
 
 const books = [
   {
@@ -28,20 +39,96 @@ const books = [
     author: "Myles Munroe",
     description: "Discover your God-given purpose and learn how to fulfill it.",
   },
+  {
+    image: "/images/book-4.jpg",
+    title: "Walking in Purpose",
+    author: "Myles Munroe",
+    description: "Discover your God-given purpose and learn how to fulfill it.",
+  },
+  {
+    image: "/images/book-4.jpg",
+    title: "Walking in Purpose",
+    author: "Myles Munroe",
+    description: "Discover your God-given purpose and learn how to fulfill it.",
+  },
+  {
+    image: "/images/book-4.jpg",
+    title: "Walking in Purpose",
+    author: "Myles Munroe",
+    description: "Discover your God-given purpose and learn how to fulfill it.",
+  },
+  {
+    image: "/images/book-4.jpg",
+    title: "Walking in Purpose",
+    author: "Myles Munroe",
+    description: "Discover your God-given purpose and learn how to fulfill it.",
+  },
+  {
+    image: "/images/book-4.jpg",
+    title: "Walking in Purpose",
+    author: "Myles Munroe",
+    description: "Discover your God-given purpose and learn how to fulfill it.",
+  },
+  {
+    image: "/images/book-4.jpg",
+    title: "Walking in Purpose",
+    author: "Myles Munroe",
+    description: "Discover your God-given purpose and learn how to fulfill it.",
+  },
 ];
 
 export default function page() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 pt-12 gap-4">
-      {books.map((book, index) => (
-        <LibraryCard
-          key={index}
-          image={book.image}
-          title={book.title}
-          author={book.author}
-          description={book.description}
-        />
-      ))}
+    <div className="bg-[#FAFAFA]">
+      <div className="container mx-auto pt-24 pb-12">
+        <div className="px-2 md:px-0 pt-2">
+          <TitleTag text="E-Library" />
+        </div>
+
+        <div className="flex flex-col md:flex-row md:justify-between md:items-center mb-4 gap-2 md:gap-0">
+          <div className="flex flex-wrap gap-4 py-4 md:py-8 px-2 md:px-0">
+            <Selection text="All books" />
+            <Selection icon={<HealingIcon />} text="Healing" />
+            <Selection icon={<FaithIcon />} text="Faith" />
+            <Selection icon={<FinanceIcon />} text="Finance" />
+            <Selection icon={<MarriageIcon />} text="Marriage & Relationship" />
+          </div>
+
+          <div className="relative w-fit md:w-[255px]">
+            <div className="absolute left-4 top-1/2 transform -translate-y-1/2 text-[#667085]">
+              <SearchIcon />
+            </div>
+
+            <input
+              type="search"
+              placeholder="Search"
+              className="text-[#667085] bg-[#FFFFFF] w-full h-8 px-12 py-2 rounded-xl border border-[#D0D5DD] focus:outline-none focus:ring-1 focus:ring-[#222357]"
+            />
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mx-auto container">
+          {books.map((book, index) => (
+            <MediaCard
+              key={index}
+              image={book.image}
+              title={book.title}
+              author={book.author}
+              description={book.description}
+              buttons={[
+                {
+                  label: "Download",
+                  icon: <DownloadIcon />,
+                  variant: "ghost",
+                  className: "bg-[#222357] text-white",
+                },
+              ]}
+            />
+          ))}
+        </div>
+      </div>
+
+      <PaginatedContent />
     </div>
   );
 }
