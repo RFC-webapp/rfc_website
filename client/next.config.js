a5750f130a098b608789d44755f499955ddbc8c4
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2025-12-19 14:02:54 +0100

    youtube stuff

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {},
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
